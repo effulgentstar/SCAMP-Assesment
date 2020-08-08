b9042ba2c5bdf446194ed92ca32f44e6db00ec97
Effulgent Star <ajahlucy@gmail.com>
2020-08-08 08:08:32 +0100

    Assesment

---
+++ Assesment.js
@@ -0,0 +1,12 @@
+var num = prompt("Enter a number");
+num = parseInt(num);
+var fib = [0, 1];
+function fibonacci(num){
+  for (var  i=0,j=1,k=0; k<num--;i=j,j=x,k++ ){
+    x=i+j;
+    fib.push(x);
+}
+}
+fibonacci(num);
+console.log ("The numbers in the sequence are " + fib);
+git remote add origin https://github.com/effulgentstar/SCAMP-Assesment.git
